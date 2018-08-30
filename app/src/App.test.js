cd39b1aad5ae1e938ebb402a0a6b87592493675c
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-30 19:46:45 -0300

    fix App eslint errors

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import './index.css';
+
+const store = createStore(
+  applyMiddleware(thunk)
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
