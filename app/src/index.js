9ebd52f73f7087ee9a0e7f3f59d9f2ee0348d5fe
Túlio de Oliveira <tulio.ao@gmail.com>
2018-09-01 18:33:34 -0300

    add persisting state functionality to app

--- app/src/index.js
+++ app/src/index.js
@@ -2,22 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers';
 import './index.css';
-import 'react-grid-layout/css/styles.css';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(thunk)
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
