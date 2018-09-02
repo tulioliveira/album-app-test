5238c15210b9e78a9719c6f63adaa145dabf514f
Túlio de Oliveira <tulio.ao@gmail.com>
2018-09-02 19:09:11 -0300

    add Overlay with "plus" icon while dragging an image over the sheet

--- app/src/containers/Sheet.js
+++ app/src/containers/Sheet.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { DropTarget } from 'react-dnd';
+import styled from 'styled-components';
+import { FiPlusCircle } from 'react-icons/fi';
 import { pushImage, renderLayout } from '../actions';
 import LayoutGrid from '../components/LayoutGrid';
+import { Text } from '../components';
+
+/**
+ * Overlay, appears on hover while dragging
+ */
+const Overlay = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
+  opacity: 1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.1s ease-in-out;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
 
 /**
  * Image target event handler
@@ -51,19 +74,63 @@ class Sheet extends Component {
     grid: PropTypes.array.isRequired
   }
 
-  render() {
-    const {
-      connectDropTarget,
-      images,
-      grid,
-      isOver
-    } = this.props;
-    console.log(isOver);
-    return connectDropTarget(
-      <div style={{ height: '100%', width: '100%', color: 'black' }}>
-        <LayoutGrid grid={grid} sheet={images} />
+  /**
+   * Render overlay if an image is being dragged over the sheet
+   */
+  renderOverlay = () => {
+    const { isOver } = this.props;
+    if (isOver) {
+      return (
+        <Overlay>
+          <FiPlusCircle size="40%" />
+        </Overlay>
+      );
+    }
+    return null;
+  }
+
+  /**
+   * Render content based on images already on the sheet
+   */
+  renderContent = () => {
+    const { grid, images } = this.props;
+    if (grid.length > 0) {
+      return (
+        <div
+          style={{
+            position: 'relative',
+            height: '100%',
+            width: '100%',
+            color: 'black',
+          }}
+        >
+          <LayoutGrid grid={grid} sheet={images} />
+          {this.renderOverlay()}
+        </div>
+      );
+    }
+    return (
+      <div
+        style={{
+          position: 'relative',
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          color: 'black',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text size="3vw" bold>Drag an image here to add it to the sheet</Text>
+        {this.renderOverlay()}
       </div>
     );
+  }
+
+  render() {
+    const { connectDropTarget } = this.props;
+
+    return connectDropTarget(this.renderContent());
   }
 }
 
